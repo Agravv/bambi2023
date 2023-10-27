6e809bfcec9253d8cc52418614eefeb0d213abbb
Csaba Varga <v.csaba22@gmail.com>
2023-10-27 20:23:00 +0200

    Refactor

--- src/main.c
+++ src/main.c
@@ -19,17 +19,28 @@ int main(void)
 {
   /* Chip errata */
   CHIP_Init();
+  SegmentLCD_Init(false);
 
   CAPLESENSE_Init(false);
   BSP_LedsInit();
   BSP_LedSet(1);
+  BSP_LedSet(0);
+
 
   /* Infinite loop */
   while (1) {
 	  int sliderPos = CAPLESENSE_getSliderPosition();
-	  if(sliderPos != -1) {
+	  if(sliderPos > 20) {
 		  BSP_LedSet(1);
-	  } else
+		  BSP_LedClear(0);
+	  }
+	  else if(sliderPos < 24 && sliderPos > -1) {
+		  BSP_LedSet(0);
 		  BSP_LedClear(1);
+	  } else {
+		  BSP_LedClear(1);
+		  BSP_LedClear(0);
   }
+
+  SegmentLCD_Number(sliderPos);}
 }

3b2777b413ac51dc994b203c2e59d2e40cb50d47
Csaba Varga <v.csaba22@gmail.com>
2023-10-28 12:12:27 +0200

    Feature: Slider IT

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "em_cmu.h"
 #include "em_lcd.h"
 #include "em_gpio.h"
+//#include "gpiointerrupt.c"
 #include "em_rtc.h"
 #include "segmentlcd.h"
 
@@ -15,6 +16,23 @@
 
 #include "bsp.h"
 
+volatile int slidePos;
+void scanCb(void) {
+	slidePos = CAPLESENSE_getSliderPosition();
+	if(slidePos > 20) {
+		BSP_LedSet(0);
+		BSP_LedClear(1);
+	} else if (slidePos < 20 && slidePos > -1) {
+		BSP_LedClear(0);
+		BSP_LedClear(1);
+	} else {
+		BSP_LedSet(1);
+		BSP_LedClear(0);
+	}
+}
+void chCb(void) {
+
+}
 
 int main(void)
 {
@@ -24,12 +42,14 @@ int main(void)
 
   CAPLESENSE_Init(false);
   BSP_LedsInit();
-  BSP_LedSet(1);
-  BSP_LedSet(0);
-  //GPIO_IntEnable(0xFFFFF);
+  BSP_LedClear(0);
+  BSP_LedClear(1);
+  CAPLESENSE_setupCallbacks(scanCb, chCb);
+
 
   /* Infinite loop */
   while (1) {
-	  LESENSE_IRQHandler();
+	  	  int pos = CAPLESENSE_getSliderPosition();
+	  	  SegmentLCD_Number(pos);
+	  }
   }
-}

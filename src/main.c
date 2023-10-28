4952a67c60aadf95079b8f8deb0342945fdc2153
Csaba Varga <v.csaba22@gmail.com>
2023-10-28 13:55:30 +0200

    Typedef

--- src/main.c
+++ src/main.c
@@ -17,43 +17,37 @@
 
 #include "bsp.h"
 
-volatile int slidePos;
-void scanCb(void) {
-	slidePos = CAPLESENSE_getSliderPosition();
-	if(slidePos > 20) {
-		BSP_LedSet(0);
-		BSP_LedClear(1);
-	} else if (slidePos < 20 && slidePos > -1) {
-		BSP_LedClear(0);
-		BSP_LedClear(1);
-	} else {
-		BSP_LedSet(1);
-		BSP_LedClear(0);
-	}
-}
-void chCb(void) {
+#include "game_handler.h"
+#include "display_snake_length.h"
+#include "game_end.h"
+#include "generate_apple.h"
 
-}
+typedef struct position {
+	uint8_t dig_pos;
+	uint8_t seg_pos;
+} position;
+
+enum dir {
+	right, left, up, down
+};
+
+position snake[37];
+uint8_t length = 1;
+enum dir direction = right;
+
+position apple;
+
+
+
 
 int main(void)
 {
   /* Chip errata */
   CHIP_Init();
-  SegmentLCD_Init(false);
 
-  CAPLESENSE_Init(false);
-  BSP_LedsInit();
-  BSP_LedClear(0);
-  BSP_LedClear(1);
-  CAPLESENSE_setupCallbacks(scanCb, chCb);
-  // EFM32GG-RM - 25.5.20 - LESENSE_IEN - Interrupt Enable Register - 16 SCANCOMPLETE - Set to enable interrupt on the SCANCOMPLETE interrupt flag
-  LESENSE_IntDisable(0x10000);
-  LESENSE_IntEnable(0x10000);
-
+  init();
 
   /* Infinite loop */
   while (1) {
-	  	  int pos = CAPLESENSE_getSliderPosition();
-	  	  SegmentLCD_Number(pos);
-	  }
+	  	  game();
   }

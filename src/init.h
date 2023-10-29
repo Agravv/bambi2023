b6dda33a8bed107c007b1d6e5a445f3832e5d28f
Csaba Varga <v.csaba22@gmail.com>
2023-10-29 16:05:32 +0100

    First Stable Version

--- src/init.h
+++ src/init.h
@@ -1,27 +1,3 @@
-//#ifndef INIT_H
-//#define INIT_H
-//
-//#include "em_device.h"
-//#include "em_chip.h"
-//#include "em_gpio.h"
-//#include "em_cmu.h"
-//#include "em_timer.h"
-//#include "segmentlcd.h"
-//#include "segmentlcd_individual.h"
-//#include "game_handler.h"
-//
-//#include "em_lesense.h"
-//#include "caplesense.h"
-//#include "game_variables.h"
-//
-//
-//void scanCb(void);
-//void chCb(void);
-//void init_elsense(void);
-//void init_lcd(void);
-//void init(void);
-//
-//#endif
 #ifndef INIT_H
 #define INIT_H
 
--- src/timer_handler.c
+++ src/timer_handler.c
@@ -1,23 +1,4 @@
-//#include "timer_handler.h"
-////extern enum dir prev_direction;
-////extern enum dir direction;
-//
-//void own_timer(int duration)
-//{
-//	LESENSE_IntEnable(0x10000);
-//
-//	Delay(10);
-//
-//	LESENSE_IntDisable(0x10000);
-//	move();
-//	//prev_direction = direction;
-//	clear_lower_lcd_screen();
-//	show_snake();
-//	show_apple();
-//}
 #include "timer_handler.h"
-//extern enum dir prev_direction;
-//extern enum dir direction;
 
 void own_timer(int duration)
 {
